300ae320f616cd261d24755f01c4bc83954eb1fb
Dylan K <dkooby3@gmail.com>
2023-11-09 14:32:25 -0500

    Finalizing Project

---
+++ javascript.js
@@ -0,0 +1,2 @@
+var notice = "DISCLAIMER: This site is 100% an academic project and only serves personal portfolion purpose, the content of this website mirrors that of my mothers personal website, bethkoobydesign.com";
+console.log(notice);
